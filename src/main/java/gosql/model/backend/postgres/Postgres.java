ba61972b09728b1ee54fcc8f634011571ec9a78a
Janani Suresh <75471874+JananiSuresh168@users.noreply.github.com>
2022-03-17 11:45:04 -0400

    connecting java with PostgreSQL and MySQL

---
+++ src/main/java/gosql/model/backend/postgres/Postgres.java
@@ -0,0 +1,113 @@
+/*
+ * ToroDB Stampede
+ * Copyright © 2016 8Kdata Technology (www.8kdata.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package gosql.config.model.backend.postgres;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.torodb.packaging.config.annotation.Description;
+import com.torodb.packaging.config.model.backend.postgres.AbstractPostgres;
+import com.torodb.packaging.config.util.ConfigUtils;
+import com.torodb.packaging.config.validation.ExistsAnyPassword;
+import com.torodb.packaging.config.validation.Host;
+import com.torodb.packaging.config.validation.Port;
+
+import javax.validation.constraints.NotNull;
+
+@JsonPropertyOrder({"host", "port", "database", "user", "password", "toropassFile",
+    "applicationName", "includeForeignKeys", "ssl"})
+@ExistsAnyPassword
+public class Postgres extends AbstractPostgres {
+
+  public Postgres() {
+    super(
+        "localhost",
+        5432,
+        "torod",
+        "torodb",
+        null,
+        ConfigUtils.getUserHomeFilePath(".toropass"),
+        "toro",
+        false, 
+        false
+    );
+  }
+
+  @NotNull
+  @Host
+  @JsonProperty(required = true)
+  @Override
+  public String getHost() {
+    return super.getHost();
+  }
+
+  @NotNull
+  @Port
+  @JsonProperty(required = true)
+  @Override
+  public Integer getPort() {
+    return super.getPort();
+  }
+
+  @NotNull
+  @JsonProperty(required = true)
+  @Override
+  public String getDatabase() {
+    return super.getDatabase();
+  }
+
+  @NotNull
+  @JsonProperty(required = true)
+  @Override
+  public String getUser() {
+    return super.getUser();
+  }
+
+  @JsonIgnore
+  @Override
+  public String getPassword() {
+    return super.getPassword();
+  }
+
+  @NotNull
+  @JsonProperty(required = true)
+  @Override
+  public String getToropassFile() {
+    return super.getToropassFile();
+  }
+
+  @NotNull
+  @JsonProperty(required = true)
+  @Override
+  public String getApplicationName() {
+    return super.getApplicationName();
+  }
+
+  @JsonIgnore
+  @Override
+  public Boolean getIncludeForeignKeys() {
+    return super.getIncludeForeignKeys();
+  }
+ 
+  @JsonProperty(required = false)
+  @NotNull
+  @Override
+  public Boolean getSsl() {
+    return super.getSsl();
+  }
+}
